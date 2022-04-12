a71b373d46a384f483f51f430ed171986483632b
jason-dou <jiachend@andrew.cmu.edu>
2022-04-12 14:25:06 -0400

    First version of exponential adaptive clipping

--- train.py
+++ train.py
@@ -17,7 +17,8 @@ import helpers
 from models import cifar, mnist
 from data import utils
 
-DEVICE =  torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def load_model(dataset: str):
     if dataset == 'cifar':
@@ -25,30 +26,30 @@ def load_model(dataset: str):
     elif dataset == 'mnist':
         return mnist.Net()
 
+
 def run_exp(exp_name, params, use_devset=False):
-    
     for benchmark in ['mnist', 'cifar']:
         print(f'Running: {exp_name}/{benchmark}')
         trainset, testset = utils.load_data(dataset=benchmark, devset=use_devset)
         trainloader = torch.utils.data.DataLoader(
             dataset=trainset, batch_size=params['batch_size'], shuffle=True, drop_last=True)
-        
-        testloader  = torch.utils.data.DataLoader(
-            dataset=testset,  batch_size=params['batch_size'], shuffle=False, drop_last=True)
+
+        testloader = torch.utils.data.DataLoader(
+            dataset=testset, batch_size=params['batch_size'], shuffle=False, drop_last=True)
 
         model = load_model(dataset=benchmark)
-        train(exp_name=f'{exp_name}/{benchmark}', model=model, trainloader=trainloader, testloader=testloader, device=DEVICE, opt_params=params)
+        train(exp_name=f'{exp_name}/{benchmark}', model=model, trainloader=trainloader, testloader=testloader,
+              device=DEVICE, opt_params=params)
 
 
 def train(
-    exp_name: str,
-    model: nn.Module,
-    trainloader: torch.utils.data.DataLoader,
-    testloader: torch.utils.data.DataLoader,
-    device: torch.device,
-    opt_params
+        exp_name: str,
+        model: nn.Module,
+        trainloader: torch.utils.data.DataLoader,
+        testloader: torch.utils.data.DataLoader,
+        device: torch.device,
+        opt_params
 ):
-
     n_epochs = opt_params['n_epochs']
     lr = opt_params['lr']
     momentum = opt_params['momentum']
@@ -72,11 +73,11 @@ def train(
 
         # Write training metrics
         writer.add_scalar(tag='Train loss', scalar_value=train_loss, global_step=epoch)
-        writer.add_scalar(tag='Train accuaracy', scalar_value=train_acc, global_step=epoch)
+        writer.add_scalar(tag='Train accuracy', scalar_value=train_acc, global_step=epoch)
 
         # Write testing metrics
         writer.add_scalar(tag='Test loss', scalar_value=test_loss, global_step=epoch)
-        writer.add_scalar(tag='Test accuaracy', scalar_value=test_acc, global_step=epoch)
+        writer.add_scalar(tag='Test accuracy', scalar_value=test_acc, global_step=epoch)
         log['epoch'].append(epoch)
         log['train_loss'].append(train_loss)
         log['train_acc'].append(train_acc)
@@ -87,21 +88,23 @@ def train(
     helpers.write_logs(exp_name, log, opt_params)
     writer.close()
 
-def train_epoch(
-    model,
-    trainloader: torch.utils.data.DataLoader,
-    device: torch.device,
-    optimizer: torch.optim,
-    criterion,
-    opt_params
-) -> List[Tuple[float, float]]:
 
-    #DP-SGD parameters
-    lr = opt_params['lr']
-    use_dp = opt_params['dp']
+def train_epoch(
+        model,
+        trainloader: torch.utils.data.DataLoader,
+        device: torch.device,
+        optimizer: torch.optim,
+        criterion,
+        opt_params
+) -> List[Tuple[float, float]]:
+    # DP-SGD parameters
+    use_adaptive = opt_params['adaptive']
     num_microbatches = opt_params['num_microbatches']
     S = opt_params['S']
     z = opt_params['z']
+    gamma = opt_params['gamma']  # Target quantile
+    lr_c = opt_params['lr_c']
+    sigma_b = 1.1 # Test value for sigma used in adaptive clipping
     sigma = z * S
 
     # Define loss and optimizer
@@ -110,7 +113,18 @@ def train_epoch(
     total = 0.0
     correct = 0.0
     for i, data in tqdm(enumerate(trainloader, 0)):
-        
+        """
+        For the batch
+        The main difference is that to maintain differential privacy, 
+        we apply a fixed C to all gradient values (in a given batch),
+        and we store a bit b specifying whether the gradient X is less than C. 
+        Then, we add gaussian noise to each b and take the average to get the b_tilde value used in their update equation
+        With this approach, don’t store the gradient norms explicitly because it could violate privacy. 
+        However, we could store the norms and track the distribution to see how it changes over time and debug.
+        """
+        # Indicator sum of gradient less than C for this batch
+        b = 0.0
+
         x, y = data[0].to(device), data[1].to(device)
         optimizer.zero_grad()
 
@@ -118,21 +132,43 @@ def train_epoch(
         loss = criterion(y_hat, y)
         running_loss += torch.mean(loss).item()
         losses = torch.mean(loss.reshape(num_microbatches, -1), dim=1)
-        
+
         saved_var = dict()
         for tensor_name, tensor in model.named_parameters():
             saved_var[tensor_name] = torch.zeros_like(tensor)
 
+        """To start with this task, 
+        it might be easiest to begin by storing the full gradient norm distribution and comparing the actual median to 
+        the median we estimate via their update equations.
+        Once this looks correct, we could use the estimated median to update the norm clipping threshold.
+        """
+        print(f"before clipping {S}")
         for sample in losses:
             sample.backward(retain_graph=True)
+
+            total_norm = 0.0
+            for p in model.parameters():
+                param_norm = p.grad.detach().data.norm(2)
+                total_norm += param_norm.item() ** 2
+            b += 1 if total_norm ** 0.5 < S else 0
+            b += torch.randn(1) * sigma_b
+
             torch.nn.utils.clip_grad_norm_(model.parameters(), S)
+
             for tensor_name, tensor in model.named_parameters():
                 new_grad = tensor.grad
                 saved_var[tensor_name].add_(new_grad)
             model.zero_grad()
 
+        b_t = b / num_microbatches
+        if use_adaptive:
+            # Linear clipping
+            S = S - lr_c * (b_t - gamma)
+            # Exponential clipping
+            # S = S * torch.exp_(-lr_c * (b_t - gamma))
+        print(f"after update clipping threshold {S}")
         for tensor_name, tensor in model.named_parameters():
-            if device.type =='cuda':
+            if device.type == 'cuda':
                 noise = torch.cuda.FloatTensor(tensor.grad.shape).normal_(0, sigma)
             else:
                 noise = torch.FloatTensor(tensor.grad.shape).normal_(0, sigma)
@@ -144,13 +180,13 @@ def train_epoch(
         _, predicted = torch.max(y_hat.data, 1)
         total += y.size(0)
         correct += (predicted == y).sum().item()
-    return running_loss/total, correct/total
+    return running_loss / total, correct / total
 
 
 def test(
-    model,
-    testloader: torch.utils.data.DataLoader,
-    device: torch.device
+        model,
+        testloader: torch.utils.data.DataLoader,
+        device: torch.device
 ) -> Tuple[float, float]:
     """Validate the network on the entire test set."""
 
@@ -171,4 +207,4 @@ def test(
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    return loss/total, correct/total
+    return loss / total, correct / total
